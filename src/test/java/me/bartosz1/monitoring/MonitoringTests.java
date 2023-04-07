0c19ae6c0510174bede454b7ea971605af2ffa64
bartosz11 <86555598+bartosz11@users.noreply.github.com>
2023-04-07 21:37:08 +0200

    Remove greenmail

--- src/test/java/me/bartosz1/monitoring/MonitoringTests.java
+++ src/test/java/me/bartosz1/monitoring/MonitoringTests.java
@@ -1,25 +1,12 @@
 package me.bartosz1.monitoring;
 
-import com.icegreen.greenmail.configuration.GreenMailConfiguration;
-import com.icegreen.greenmail.junit5.GreenMailExtension;
-import com.icegreen.greenmail.util.ServerSetup;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 public class MonitoringTests {
-
-    @RegisterExtension
-    static GreenMailExtension greenMailExtension;
-
-    static {
-        final ServerSetup SERVER_SETUP = ServerSetup.SMTP;
-        SERVER_SETUP.setServerStartupTimeout(10000);
-        greenMailExtension = new GreenMailExtension(SERVER_SETUP).withConfiguration(GreenMailConfiguration.aConfig().withUser("bmonitord-test", "Test1234")).withPerMethodLifecycle(false);
-    }
 
     @Test
     public void contextLoads() {
