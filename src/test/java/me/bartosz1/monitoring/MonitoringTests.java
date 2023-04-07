02e542f477f83be63c30960cb049559f0a6ad27d
bartosz11 <86555598+bartosz11@users.noreply.github.com>
2023-04-07 21:29:48 +0200

    it's actually greenmail's fault

--- src/test/java/me/bartosz1/monitoring/MonitoringTests.java
+++ src/test/java/me/bartosz1/monitoring/MonitoringTests.java
@@ -13,7 +13,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class MonitoringTests {
 
     @RegisterExtension
-    static GreenMailExtension greenMailExtension = new GreenMailExtension(ServerSetup.SMTP).withConfiguration(GreenMailConfiguration.aConfig().withUser("bmonitord-test", "Test1234")).withPerMethodLifecycle(false);
+    static GreenMailExtension greenMailExtension;
+
+    static {
+        final ServerSetup SERVER_SETUP = ServerSetup.SMTP;
+        SERVER_SETUP.setServerStartupTimeout(10000);
+        greenMailExtension = new GreenMailExtension(SERVER_SETUP).withConfiguration(GreenMailConfiguration.aConfig().withUser("bmonitord-test", "Test1234")).withPerMethodLifecycle(false);
+    }
 
     @Test
     public void contextLoads() {
