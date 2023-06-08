b89d54f5fc22e00d7a1b55ca2143b7a0f0566e92
bartosz11 <86555598+bartosz11@users.noreply.github.com>
2023-06-08 20:08:25 +0200

    security config rewrite to new lambda dsl

--- src/main/java/me/bartosz1/monitoring/security/SecurityConfig.java
+++ src/main/java/me/bartosz1/monitoring/security/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -26,32 +28,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //CSRF will get enabled once I start doing frontend I think
-        http.cors().and().csrf().disable().authorizeHttpRequests()
-                //Frontend requests
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/index.html").permitAll()
-                //These aren't related to API, just some routes that frontend might utilize if it doesn't use a hash router (stock one does)
-                .requestMatchers("/dashboard/**", "/auth/**", "/report/**", "/statuspage/**").permitAll()
-                .requestMatchers("/assets/**").permitAll()
-                //All login and register requests need to be allowed
-                .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
-                //Allow all requests agents make - install signal, post data
-                .requestMatchers("/api/agent/**").permitAll()
-                //Public data
-                .requestMatchers("/api/heartbeat/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/monitor/{id}").permitAll()
-                .requestMatchers("/api/monitor/{monitorId}/agent").permitAll()
-                .requestMatchers("/api/incident/**").permitAll()
-                .requestMatchers("/api/statuspage/{id}/public").permitAll()
-                //Allow all requests to Actuator endpoints - the only one exposed is /health
-                .requestMatchers("/app/**").permitAll()
-                //springdoc
-                .requestMatchers("/api-doc/**").permitAll()
-                //Require authentication for all other requests
-                .anyRequest().authenticated().and()
-                //Use stateless sessions
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable).sessionManagement(sessionMgmt -> sessionMgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        //frontend requests
+                        .requestMatchers("/", "/index.html").permitAll()
+                        //some routes that may be utilized by client side routers on frontend
+                        .requestMatchers("/dashboard/**", "/auth/**", "/report/**", "/statuspage/**").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                        //all login and register requests need to be allowed obviously
+                        .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
+                        //allow all requests agent make - post data, install signal etc
+                        .requestMatchers("/api/agent/**").permitAll()
+                        //public data
+                        .requestMatchers("/api/heartbeat/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/monitor/{id}").permitAll()
+                        .requestMatchers("/api/monitor/{monitorId}/agent").permitAll()
+                        .requestMatchers("/api/incident/**").permitAll()
+                        .requestMatchers("/api/statuspage/{id}/public").permitAll()
+                        //actuator requests
+                        .requestMatchers("/app/**").permitAll()
+                        //springdoc
+                        .requestMatchers("/api-doc/**").permitAll()
+                        .anyRequest().authenticated());
+
         //Add our custom JWT filter
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         //It can be placed anywhere
