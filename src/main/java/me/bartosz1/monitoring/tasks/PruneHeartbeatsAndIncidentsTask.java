64359d35b56e29652c50a882d69805a6db79ddbc
bartosz11 <86555598+bartosz11@users.noreply.github.com>
2023-07-23 00:36:32 +0200

    impr: prune task uses raw JDBC now, most likely resulting in a smaller (or nonexistent) RAM usage spike on start aka: there's no conversion from ResultSet to "Hibernate-watched" Java POJO's which take a lot of memory just to get deleted a second later overall performance of the task should be better, as all queries are all "bulk queries", not one-per-row

--- src/main/java/me/bartosz1/monitoring/tasks/PruneHeartbeatsAndIncidentsTask.java
+++ src/main/java/me/bartosz1/monitoring/tasks/PruneHeartbeatsAndIncidentsTask.java
@@ -1,39 +1,29 @@
 package me.bartosz1.monitoring.tasks;
 
 import jakarta.transaction.Transactional;
-import me.bartosz1.monitoring.models.Heartbeat;
-import me.bartosz1.monitoring.models.Incident;
-import me.bartosz1.monitoring.models.Monitor;
-import me.bartosz1.monitoring.repositories.HeartbeatRepository;
-import me.bartosz1.monitoring.repositories.IncidentRepository;
-import me.bartosz1.monitoring.repositories.MonitorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.time.Instant;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.StreamSupport;
 
 @Component
 public class PruneHeartbeatsAndIncidentsTask {
 
-    private final HeartbeatRepository heartbeatRepository;
-    private final IncidentRepository incidentRepository;
-    private final MonitorRepository monitorRepository;
+    private final DataSource dataSource;
     private final int pruneDays;
     private final boolean runGC;
     private static final Logger LOGGER = LoggerFactory.getLogger(PruneHeartbeatsAndIncidentsTask.class);
 
-    public PruneHeartbeatsAndIncidentsTask(HeartbeatRepository heartbeatRepository, IncidentRepository incidentRepository, MonitorRepository monitorRepository, @Value("${monitoring.prune.age:14}") int pruneDays, @Value("${monitoring.run-gc-after-tasks:false}") boolean runGC) {
-        this.heartbeatRepository = heartbeatRepository;
-        this.incidentRepository = incidentRepository;
-        this.monitorRepository = monitorRepository;
+    public PruneHeartbeatsAndIncidentsTask(DataSource dataSource, @Value("${monitoring.prune.age:14}") int pruneDays, @Value("${monitoring.run-gc-after-tasks:false}") boolean runGC) {
+        this.dataSource = dataSource;
         this.pruneDays = pruneDays;
         this.runGC = runGC;
     }
@@ -41,36 +31,101 @@ public class PruneHeartbeatsAndIncidentsTask {
     @Scheduled(fixedDelayString = "${monitoring.prune.delay:86400}", timeUnit = TimeUnit.SECONDS)
     @Transactional
     public void cleanup() {
-        //not sure about unresponsiveness
-        LOGGER.info("Starting prune of old heartbeats and incidents, app might become unresponsive. NEXT MESSAGE FROM THIS TASK DOESN'T INDICATE IT'S DONE");
-        long min = Instant.now().getEpochSecond() - pruneDays * 86400L;
-        LOGGER.debug("Using " + min + " as min timestamp");
-        Set<Monitor> bulkUpdateMonitors = new HashSet<>();
-        //delete all orphans i guess
-        heartbeatRepository.deleteAllByMonitorIsNull();
-        incidentRepository.deleteAllByMonitorIsNull();
-        Iterable<Heartbeat> targetHeartbeats = heartbeatRepository.findAllByTimestampLessThan(min);
-        List<Incident> targetIncidents = StreamSupport.stream(incidentRepository.findAllByStartTimestampLessThan(min).spliterator(), false).filter(incident -> !incident.isOngoing()).toList();
-        targetHeartbeats.forEach(hb -> {
-            //how to spam debug logs 101
-            LOGGER.debug("Processing heartbeat ID " + hb.getId() + " monitor " + hb.getMonitor().getId());
-            //unbind from monitor
-            Monitor monitor = hb.getMonitor();
-            monitor.getHeartbeats().remove(hb);
-            bulkUpdateMonitors.add(monitor);
-        });
-        targetIncidents.forEach(incident -> {
-            LOGGER.debug("Processing incident ID " + incident.getId() + " monitor " + incident.getMonitor().getId());
-            Monitor monitor = incident.getMonitor();
-            monitor.getIncidents().remove(incident);
-            bulkUpdateMonitors.add(monitor);
-        });
-        //monitor cascades all operation to incident and heartbeat objects
-        monitorRepository.saveAll(bulkUpdateMonitors);
-        LOGGER.info("At least " + targetHeartbeats.spliterator().getExactSizeIfKnown() + " heartbeats, " + targetIncidents.size() + " incidents affected");
-        if (runGC) {
-            LOGGER.info("Running JVM garbage collector.");
-            System.gc();
+        try {
+            //NOTE: before you scream at me for not using prepared statements everywhere there's no user-input data
+            //all is generated DB-side and no result is returned to users
+            //not sure about unresponsiveness
+            LOGGER.info("Starting prune of old heartbeats and incidents, app might become unresponsive. NEXT MESSAGE FROM THIS TASK DOESN'T INDICATE IT'S DONE");
+            long min = Instant.now().getEpochSecond() - pruneDays * 86400L;
+            Connection connection = dataSource.getConnection();
+            LOGGER.debug("Using " + min + " as min timestamp");
+            //get target incidents
+            PreparedStatement incidentsSelectStatement = connection.prepareStatement("SELECT id FROM incidents WHERE start_timestamp < ? AND ongoing = false;");
+            incidentsSelectStatement.setLong(1, min);
+            incidentsSelectStatement.closeOnCompletion();
+            List<Long> incidentIdsList = convertIdResultSetToIdList(incidentsSelectStatement.executeQuery());
+            //get target heartbeats
+            PreparedStatement heartbeatsSelectStatement = connection.prepareStatement("SELECT id FROM heartbeats WHERE timestamp < ?;");
+            heartbeatsSelectStatement.setLong(1, min);
+            heartbeatsSelectStatement.closeOnCompletion();
+            List<Long> heartbeatIdsList = convertIdResultSetToIdList(heartbeatsSelectStatement.executeQuery());
+            //delete all orphans first i guess
+            Statement delStmt = connection.createStatement();
+            delStmt.execute("DELETE FROM incidents WHERE monitor_id = NULL;");
+            delStmt.execute("DELETE FROM heartbeats WHERE monitor_id = NULL;");
+            if (!heartbeatIdsList.isEmpty()) {
+                //delete from monitors_heartbeats
+                String monitorsHbsDeleteQuery = prepareMonitorsHeartbeatsDeleteQuery(heartbeatIdsList);
+                System.out.println(monitorsHbsDeleteQuery);
+                delStmt.execute(monitorsHbsDeleteQuery);
+                //delete from heartbeats
+                String hbDeleteQuery = prepareEntityDeleteQuery(heartbeatIdsList, true);
+                System.out.println(hbDeleteQuery);
+                delStmt.execute(hbDeleteQuery);
+            }
+            if (!incidentIdsList.isEmpty()) {
+                //delete from monitors_incidents
+                String monitorsIncsDeleteQuery = prepareMonitorsIncidentsDeleteQuery(incidentIdsList);
+                System.out.println(monitorsIncsDeleteQuery);
+                delStmt.execute(monitorsIncsDeleteQuery);
+                //delete from incidents
+                String incDeleteQuery = prepareEntityDeleteQuery(incidentIdsList, false);
+                System.out.println(incDeleteQuery);
+                delStmt.execute(incDeleteQuery);
+            }
+            //finished
+            delStmt.close();
+            LOGGER.info("At least " + heartbeatIdsList.size() + " heartbeats, " + incidentIdsList.size() + " incidents affected");
+            //todo: remove this useless "feature"
+            if (runGC) {
+                LOGGER.info("Running JVM garbage collector.");
+                System.gc();
+            }
+        } catch (SQLException e) {
+            LOGGER.error("SQLException while pruning DB!");
+            e.printStackTrace();
         }
+    }
+
+    private List<Long> convertIdResultSetToIdList(ResultSet resultSet) throws SQLException {
+        List<Long> ids = new ArrayList<>();
+        while (resultSet.next()) {
+            ids.add(resultSet.getLong("id"));
+        }
+        resultSet.close();
+        return ids;
+    }
+
+    private String prepareMonitorsHeartbeatsDeleteQuery(List<Long> ids) {
+        StringBuilder sb = new StringBuilder().append("DELETE FROM monitors_heartbeats WHERE heartbeats_id IN (");
+        for (int i = 0; i < ids.size(); i++) {
+            long id = ids.get(i);
+            sb.append(id);
+            if (ids.size() - 1 == i) sb.append(");");
+            else sb.append(", ");
+        }
+        return sb.toString();
+    }
+
+    private String prepareMonitorsIncidentsDeleteQuery(List<Long> ids) {
+        StringBuilder sb = new StringBuilder().append("DELETE FROM monitors_incidents WHERE incidents_id IN (");
+        for (int i = 0; i < ids.size(); i++) {
+            long id = ids.get(i);
+            sb.append(id);
+            if (ids.size() - 1 == i) sb.append(");");
+            else sb.append(", ");
+        }
+        return sb.toString();
+    }
+
+    private String prepareEntityDeleteQuery(List<Long> ids, boolean heartbeat) {
+        StringBuilder sb = new StringBuilder().append("DELETE FROM ").append(heartbeat ? "heartbeats" : "incidents").append(" WHERE id IN (");
+        for (int i = 0; i < ids.size(); i++) {
+            long id = ids.get(i);
+            sb.append(id);
+            if (ids.size() - 1 == i) sb.append(");");
+            else sb.append(", ");
+        }
+        return sb.toString();
     }
 }
